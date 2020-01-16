55705d2566f33483102d2ba13e130fe39a35350d
Larissa <larissa.nguyen@yale.edu>
2020-01-16 17:01:39 -0500

    guide

--- guide/urls.py
+++ guide/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-<<<<<<< HEAD
 from .views import homePageView,aboutPageView,guideView,guideTopics,guideOverviewView,guideEventView, jobAppView, guideReccomendationsView, contactPageView
-=======
-from .views import homePageView,aboutPageView,guideView,guideOverviewView,guideEventView, jobAppView, guideReccomendationsView, contactPageView, guideProjects, guideFields
->>>>>>> 6ea846bbc2a53e5d98fab2cf31c5313cea0fb602
 
 from . import views
 
@@ -14,12 +10,7 @@ urlpatterns = [
     path('/contact', contactPageView, name='contact'),
     path('/guide', guideView, name='guide'),
     path('/guide/overview', guideOverviewView, name='guideoverview'),
-<<<<<<< HEAD
     path('/guide/topics', guideTopics, name='guidetopics'),
-=======
-    path('/guide/fields', guideFields, name='guidefields'),
-    path('/guide/projects', guideProjects, name='guideprojects'),
->>>>>>> 6ea846bbc2a53e5d98fab2cf31c5313cea0fb602
     path('/guide/reccomendations', guideReccomendationsView, name='reccomendations'),
     path('/guide/guideevents', guideEventView, name='guideEvents'),
     path('/job_app', views.jobAppView, name = 'job_app')
